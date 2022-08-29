10e2f4aa903a7ba6436432fd6764cc9ffddcb054
Jörg Kullrich <osvald@gmx.de>
2022-08-29 16:35:25 +0200

    adapt styling with animation

--- app.js
+++ app.js
@@ -43,11 +43,12 @@ function deleteCheck(e) {
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
     todo.classList.add("fall");
-    todo.remove();
-    };
-
-    if (item.classList[0] === "complete-btn") {
-      const todo = item.parentElement;
-      todo.classList.toggle("completed");
-    }
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
